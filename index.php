3cb3da38edb4a60106ce03146a5ac1791c7b693e
fatma Gelil <gelilfatma79@gmail.com>
2023-03-20 00:27:20 +0200

    progress

--- index.php
+++ index.php
@@ -1,9 +1,45 @@
 <?php
 class car{
+//properties
+  public $version;
+  public $color;
+  public $speed;
+  public function features(){
+      echo 'car color is '. $this->color;
+      echo '<br>';
+      echo 'car version is '. $this->version;
+      echo '<br>';
+      echo 'car speed is '. $this->speed;
+      echo '<br>';
+  }
+  public function owner($name){
+      if (strlen($this->$name)<3){
+          echo 'invalid name, please enter your real name  <br>';
+      }else{
+          echo 'welcome '.$name;
+      }
+
+
+  }
 
 }
 $KIA=new car();
+echo '<br>';
+$KIA->color='red';
+$KIA->speed='200km/hr';
+$KIA->version='2018';
+$KIA->features();
+$KIA->owner('fatma');
+echo '<br>';
+$honda=new car();
+$honda->color='white';
+$honda->speed='400km/hr';
+$honda->version='2019';
+$honda->features();
+$honda->owner('ma');
 echo '<pre>';
-echo 'your dream';
 var_dump($KIA);
+echo '<br>';
+var_dump($honda);
+
 echo '</pre>';
