4ff277fc83cd875b4d997cfb3030dfbbdf319600
fatma Gelil <gelilfatma79@gmail.com>
2023-03-21 02:52:12 +0200

    progress

--- index.php
+++ index.php
@@ -4,17 +4,25 @@ class car{
   public $version;
   public $color;
   public $speed;
-
-  public function features(){
+  private $lock;
+public function changes($v,$c,$s){
+    $this->version=$v;
+    $this->color=$c;
+    $this->speed=$s;
+}
+public function changelock($lo){
+    $this->lock=$lo;
+}
+  /*public function features(){
       echo 'car color is '. $this->color;
       echo '<br>';
       echo 'car version is '. $this->version;
       echo '<br>';
       echo 'car speed is '. $this->speed;
       echo '<br>';
-  }
-  const OWNAME = 4;
-  public function owner($name){
+  }*/
+  //const OWNAME = 4;
+/*  public function owner($name){
       if (strlen($this->$name)<self::OWNAME){
           echo 'invalid name, please enter your real name  <br>';
       }else{
@@ -22,23 +30,27 @@ class car{
       }
 
 
-  }
+  }*/
 
 }
 $KIA=new car();
 echo '<br>';
-$KIA->color='red';
-$KIA->speed='200km/hr';
-$KIA->version='2018';
-$KIA->features();
-$KIA->owner('fatma');
+//$KIA->changes('blue');
+//$KIA->speed='200km/hr';
+$KIA->changes('2GB','blue','300km/hr');
+$KIA->changelock('123#');
+//$KIA->features();
+//$KIA->owner('fatma');
 echo '<br>';
 $honda=new car();
 $honda->color='white';
 $honda->speed='400km/hr';
 $honda->version='2019';
-$honda->features();
-$honda->owner('ma');
+$honda->changelock('123@');
+//$honda->lock='123@';
+//echo $honda->lock;
+//$honda->features();
+//$honda->owner('ma');
 echo '<pre>';
 var_dump($KIA);
 echo '<br>';
